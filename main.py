96018c82d4842c73535295902466258b50a75143
Boyoung Kim <olel07@naver.com>
2018-12-17 19:47:42 +0900

    주석 추가

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import re
-import matplotlib.pyplot as plt
 import cv2
-from matplotlib.image import imread
 from random import *
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-#import libraries
-#1. matplotlib
-#2. opencv-python
-#3. beautifulSoup
+# import libraries
+# 1. opencv-python
+# 2. beautifulSoup
 
+# 게임 소개 클래스
 class BaseInfo:
     def __init__(self):
         self.subnum = ""
@@ -38,7 +36,7 @@ class BaseInfo:
         self.setsubnum(input("> 내가 정한 주제는 : "))
         print(self.line)
 
-
+# 게임 시작 클래스
 class StartGame:
     def __init__(self):
         self.title = ""
@@ -57,7 +55,8 @@ class StartGame:
         else:
             return str(c.group())
 
-    def crawltofranchise(self):  # 1. 식당 방문
+    # 1. 식당 방문 타이틀 세팅
+    def crawltofranchise(self):  
         html = urlopen("https://www.businessinsider.com/top-food-franchises-in-america-2016-1#15-baskin-robbins-2")
         bsobj = BeautifulSoup(html.read(), "html.parser")
         section = bsobj.find("div", {"class": "slide-title clearfix"})  # 타이틀을 포함하고 있는 h2 태그 찾아옴
@@ -77,6 +76,7 @@ class StartGame:
         self.answer = self.answer.lower()
         self.ansList = list(self.answer)
 
+    # 2. 영화 타이틀 세팅
     def crawltomovie(self):
         html = urlopen("https://www.rottentomatoes.com/top/bestofrt/top_100_action__adventure_movies/")
         bsobj = BeautifulSoup(html.read(), "html.parser")
@@ -96,7 +96,7 @@ class StartGame:
         self.answer = self.answer.lower()
         self.ansList = list(self.answer)
 
-
+# 게임 진행 클래스
 class PlayGame:
     def __init__(self):
         self.title = ""
@@ -120,11 +120,12 @@ class PlayGame:
         print("힌트 : ")
         print(self.cusList)
 
-    def sethanggirl(self):#행걸 이미지 출력용
+    # 행걸 이미지 출력용
+    def sethanggirl(self):
         self.hanggirl = 'hanggirl' + str(self.hangnum) + '.png'
         self.hangnum = self.hangnum + 1
 
-
+    #  답지에 알파벳작성
     def updatecuslist(self, alpha):
         print("남은 LIFE ", "★"*(self.life-1))
         alpha = alpha.lower()
@@ -133,19 +134,24 @@ class PlayGame:
 
         r = re.compile('[a-z]')
         match = r.search(alpha)
-        if(match == None):
+        if(match is None):
             print("영어만 입력하세요.")
             return False
+        
+        # 정답 내 모든 알파벳 찾기 위함
+        while x != -1:
 
-        while x != -1: #정답 내 모든 알파벳 찾기 위함
             x = self.answer.find(alpha)  # 단어 위치
             if x != -1:
-                self.answer = self.answer[0:x] + '_' + self.answer[x+1:] #중복검사 방지
+                #  중복검사 방지
+                self.answer = self.answer[0:x] + '_' + self.answer[x+1:]
                 self.cusList[x] = alpha
                 idx = idx + 1
         print(self.cusList)
         if idx == 0:
-            self.life = self.life - 1 #알파벳이 포함되어 있지 않으면 life 깎기
+            #  알파벳이 포함되어 있지 않으면 life 깎기
+            self.life = self.life - 1
+            # 행걸 이미지 출력(1번부터 6번까지)
             self.sethanggirl()
             img = cv2.imread(self.hanggirl,0)
             cv2.namedWindow(self.hanggirl, cv2.WINDOW_NORMAL)
@@ -153,14 +159,18 @@ class PlayGame:
             cv2.waitKey(2000)
             cv2.destroyAllWindows()
 
+
+# 클래스 선언
 an = BaseInfo()
 st = StartGame()
 pl = PlayGame()
 sucess = False
 
+# 게임 인트로
 an.gameintro()
 
 
+# 메뉴 선택
 if an.subnum == '0':
     print("두 개의 주제 중 하나를 골라서 타이틀을 맞추는 게임입니다.")
     print("힌트를 보고 몇 글자의 알파벳인지 확인하세요.")
@@ -169,28 +179,34 @@ if an.subnum == '0':
     quit()
 elif an.subnum == '1':
     an.setsubject("식당 방문")
-    print(an.subject + an.intro)#인사말
+    # 인사말
+    print(an.subject + an.intro)
     print(an.line)
-    st.crawltofranchise() #식당 타이틀
+    # 식당 타이틀 세팅하기
+    st.crawltofranchise()
 elif an.subnum == '2':
     an.setsubject("영화 보기")
-    print(an.subject + an.intro)  # 인사말
+    # 인사말
+    print(an.subject + an.intro)
     print(an.line)
-    st.crawltomovie() #영화 타이틀
+    # 영화 타이틀 세팅하기
+    st.crawltomovie()
 elif an.subnum == '3':
-    print("좋은 꿈 꾸세요^~^")  # 인사말
+    print("좋은 꿈 꾸세요^~^")
     print(an.line)
     quit()
 
-#게임진행
-pl.setanswer(st.answer)#papa john's
-pl.setansList(st.ansList)#['p', 'a', 'p', 'a', ' ', 'j', 'o', 'h', 'n', "'", 's']
+#  게임 진행
+pl.setanswer(st.answer)  
+pl.setansList(st.ansList) 
 pl.setcuslist(st.ansList)
+# 사용자에게 알파벳 입력 받기
 while pl.ansList != pl.cusList and pl.life > 0:
     print(an.line)
     pl.updatecuslist(input("> 알파벳입력 : "))
     print(an.line)
 
+#  게임 결과
 if pl.life == 0:
     print("게임에서 졌습니다.")
 else:
